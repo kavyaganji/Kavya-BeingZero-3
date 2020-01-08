37c1e5ce3b5101e0e4ac5524391512f7d27a8e2d
Kavya Ganji <kavyaganji1901@gmail.com>
2020-01-08 12:43:44 +0530

    Users

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  users;
 
-  ngOnInit() {
+  constructor( private data: DataService) { }
+
+  ngOnInit() 
+  {
+    this.data.getUsers().subscribe(d => 
+      {
+      this.users = d;
+      })
   }
-
 }
